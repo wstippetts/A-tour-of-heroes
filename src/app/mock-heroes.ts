d8b1f29d3b7b2ca33fdb78ef1a34f6db743d043e
Scott Tippetts <wstippetts+github@gmail.com>
2023-04-21 12:03:10 -0600

    lunch time

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magenta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  { id: 100, name: 'Captain Underpants' },
+  { id: 9000, name: 'General Zod' },
+  { id: 9001, name: 'Super Man' }
+]
